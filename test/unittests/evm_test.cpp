cce6ab9f749dd4cd4eb559c34b98f54b25aa6857
rodiazet <radek.zagorowicz@gmail.com>
2022-11-28 12:22:23 +0100

    test: Add EIP-663 unit tests

--- test/unittests/evm_test.cpp
+++ test/unittests/evm_test.cpp
@@ -153,6 +153,157 @@ TEST_P(evm, swapsn_push)
     EXPECT_STATUS(EVMC_BAD_JUMP_DESTINATION);
 }
 
+TEST_P(evm, dupn)
+{
+    // DUPN is not implemented in Advanced.
+    if (evm::isAdvanced())
+        return;
+
+    rev = EVMC_CANCUN;
+
+    auto pushes = bytecode{};
+    for (uint64_t i = 1; i <= 20; ++i)
+        pushes += push(i);
+
+    execute(pushes + OP_DUPN + "00" + ret_top());
+    EXPECT_STATUS(EVMC_SUCCESS);
+    EXPECT_OUTPUT_INT(4);
+
+    execute(pushes + OP_DUPN + "02" + ret_top());
+    EXPECT_STATUS(EVMC_SUCCESS);
+    EXPECT_OUTPUT_INT(2);
+
+    execute(pushes + OP_DUPN + "03" + ret_top());
+    EXPECT_STATUS(EVMC_SUCCESS);
+    EXPECT_OUTPUT_INT(1);
+
+    execute(pushes + OP_DUPN + "04" + ret_top());
+    EXPECT_STATUS(EVMC_STACK_UNDERFLOW);
+}
+
+TEST_P(evm, swapn)
+{
+    // SWAPN is not implemented in Advanced.
+    if (evm::isAdvanced())
+        return;
+
+    rev = EVMC_CANCUN;
+
+    auto pushes = bytecode{};
+    for (uint64_t i = 1; i <= 20; ++i)
+        pushes += push(i);
+
+    execute(pushes + OP_SWAPN + "00" + ret_top());
+    EXPECT_STATUS(EVMC_SUCCESS);
+    EXPECT_OUTPUT_INT(3);
+
+    execute(pushes + OP_SWAPN + "01" + ret_top());
+    EXPECT_STATUS(EVMC_SUCCESS);
+    EXPECT_OUTPUT_INT(2);
+
+    execute(pushes + OP_SWAPN + "02" + ret_top());
+    EXPECT_STATUS(EVMC_SUCCESS);
+    EXPECT_OUTPUT_INT(1);
+
+    execute(pushes + OP_SWAPN + "03" + ret_top());
+    EXPECT_STATUS(EVMC_STACK_UNDERFLOW);
+}
+
+TEST_P(evm, dupn_full_stack)
+{
+    // DUPN is not implemented in Advanced.
+    if (evm::isAdvanced())
+        return;
+
+    rev = EVMC_CANCUN;
+    auto full_stack_code = bytecode{};
+    for (uint64_t i = 1023; i >= 1; --i)
+        full_stack_code += push(i);
+
+    execute(full_stack_code + OP_POP + OP_DUPN + "00" + ret_top());
+    EXPECT_STATUS(EVMC_SUCCESS);
+    EXPECT_OUTPUT_INT(17 + 1);
+
+    execute(full_stack_code + OP_POP + OP_DUPN + "ff" + ret_top());
+    EXPECT_STATUS(EVMC_SUCCESS);
+    EXPECT_OUTPUT_INT(255 + 17 + 1);
+
+    execute(full_stack_code + OP_POP + OP_DUPN + "fe" + ret_top());
+    EXPECT_STATUS(EVMC_SUCCESS);
+    EXPECT_OUTPUT_INT(254 + 17 + 1);
+
+    execute(full_stack_code + OP_DUPN + "fe" + OP_DUPN + "ff");
+    EXPECT_STATUS(EVMC_STACK_OVERFLOW);
+}
+
+TEST_P(evm, swapn_full_stack)
+{
+    // SWAPN is not implemented in Advanced.
+    if (evm::isAdvanced())
+        return;
+
+    rev = EVMC_CANCUN;
+    auto full_stack_code = bytecode{};
+    for (uint64_t i = 1024; i >= 1; --i)
+        full_stack_code += push(i);
+
+    execute(full_stack_code + OP_POP + OP_SWAPN + "00" + ret_top());
+    EXPECT_STATUS(EVMC_SUCCESS);
+    EXPECT_OUTPUT_INT(17 + 2);
+
+    execute(full_stack_code + OP_POP + OP_SWAPN + "ff" + ret_top());
+    EXPECT_STATUS(EVMC_SUCCESS);
+    EXPECT_OUTPUT_INT(255 + 17 + 2);
+
+    execute(full_stack_code + OP_POP + OP_SWAPN + "fe" + ret_top());
+    EXPECT_STATUS(EVMC_SUCCESS);
+    EXPECT_OUTPUT_INT(254 + 17 + 2);
+
+    execute(full_stack_code + OP_SWAPN + "ff" + OP_SWAPN + "00" + OP_RETURN);
+    EXPECT_STATUS(EVMC_SUCCESS);
+    EXPECT_EQ(result.output_size, 2);
+}
+
+TEST_P(evm, dupn_dup_consistency)
+{
+    // DUPN is not implemented in Advanced.
+    if (evm::isAdvanced())
+        return;
+
+    rev = EVMC_CANCUN;
+    auto pushes = bytecode{};
+    for (uint64_t i = 32; i >= 1; --i)
+        pushes += push(i);
+
+    execute(pushes + OP_DUP16 + ret_top());
+    EXPECT_STATUS(EVMC_SUCCESS);
+    EXPECT_OUTPUT_INT(16);
+
+    execute(pushes + OP_DUPN + "00" + ret_top());
+    EXPECT_STATUS(EVMC_SUCCESS);
+    EXPECT_OUTPUT_INT(17);
+}
+
+TEST_P(evm, swapn_swap_consistency)
+{
+    // DUPN is not implemented in Advanced.
+    if (evm::isAdvanced())
+        return;
+
+    rev = EVMC_CANCUN;
+    auto pushes = bytecode{};
+    for (uint64_t i = 32; i >= 1; --i)
+        pushes += push(i);
+
+    execute(pushes + OP_SWAP16 + ret_top());
+    EXPECT_STATUS(EVMC_SUCCESS);
+    EXPECT_OUTPUT_INT(17);
+
+    execute(pushes + OP_SWAPN + "00" + ret_top());
+    EXPECT_STATUS(EVMC_SUCCESS);
+    EXPECT_OUTPUT_INT(18);
+}
+
 TEST_P(evm, gas)
 {
     execute(40, "5a5a5a010160005360016000f3");
